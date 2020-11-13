c0e2443f140cbedc3366060552d02968b57e02bd
Neil Johari <neil@johari.tech>
2020-11-13 13:59:49 -0500

    Remove const requirement for sumOfEvenGrandparents

--- problem.cpp
+++ problem.cpp
@@ -20,19 +20,23 @@ struct Node {
 /*
   REQUIRES: nothing. root may be null
   MODIFIES: nothing
+  EFFECTS: see the problem statement. this is the function you are implementing
+  to return the sum of nodes with even-valued grandparents.
 */
-int sumOfEvenGrandparents(const Node* root) {
-  // TODO: COMPLETE YOUR IMPLEMENTATION 
-  // Feel free to add helper functions as needed. Do not alter the definition of Node.
+int sumOfEvenGrandparents(Node *root) {
+  // TODO: COMPLETE YOUR IMPLEMENTATION
+  // Feel free to add helper functions as needed. Do not alter the definition of
+  // Node.
 
-  return 0; // this line is included in the starter code for compilation purposes. feel free to remove it!
+  return 0; // this line is included in the starter code for compilation
+            // purposes. feel free to remove it!
 }
 
 // Driver code to test your solution
 int main() {
   // Create and print the tree from the sample input
   vector<int> data = {1, 2, 3, 4, 5, 6, 6, 6, 6, 6};
-  Node* root = constructBinaryTreeFromLevelOrder(data, root, 0);
+  Node *root = constructBinaryTreeFromLevelOrder(data, root, 0);
   print_diagram(root);
 
   cout << "Your answer: " << sumOfEvenGrandparents(root) << '\n';
